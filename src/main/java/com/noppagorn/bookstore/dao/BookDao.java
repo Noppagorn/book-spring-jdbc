a765eab5c52ffbef88b29a599d4d6a888e768f33
NoppaGorn <tongnop2540@gmail.com>
2019-11-23 12:26:29 +0700

    finish

---
+++ src/main/java/com/noppagorn/bookstore/dao/BookDao.java
@@ -0,0 +1,19 @@
+package com.noppagorn.bookstore.dao;
+
+import me.iampeach.bookstore.model.Book;
+
+import java.util.List;
+
+public interface BookDao {
+    void save(Book book);
+
+    void update(int id, Book book);
+
+    void deleteById(int id);
+
+    Book findById(int id);
+
+    ArrayList<Book> findAll();
+
+
+}
